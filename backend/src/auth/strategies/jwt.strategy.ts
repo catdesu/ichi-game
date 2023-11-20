891d0941a2deb03e50331ac06c76ea22ec7fc4d0
Cat Sama <david.aeschlimann96@gmail.com>
2023-11-20 11:09:19 +0100

    Removed refactor mistake

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: `${`${process.env.SECRET_KEY}`}`,
+      secretOrKey: `${process.env.SECRET_KEY}`,
     });
   }
 
