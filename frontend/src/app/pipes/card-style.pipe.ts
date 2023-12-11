4c902eb10a1e627c3b10edbdba3d7abbec6a02b5
Cat Sama <david.aeschlimann96@gmail.com>
2023-12-11 13:01:57 +0100

    Removed dynamic image with static images

--- frontend/src/app/pipes/card-style.pipe.ts
+++ frontend/src/app/pipes/card-style.pipe.ts
@@ -5,13 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CardStylePipe implements PipeTransform {
 
-  transform(cardName: string, playableCards: string[], isTopCard: boolean): object {
+  transform(cardName: string, playableCards: string[]): object {
     const isPlayable = playableCards.includes(cardName);
 
     return {
-      'background': `url("../../../assets/images/cards-front/${cardName}.png") center/cover`,
-      'color': 'transparent',
-      'filter': isPlayable ? 'none' : ( isTopCard ? 'none' : 'brightness(50%)')
+      'filter': isPlayable ? 'none' : 'brightness(50%)'
     };
   }
 
